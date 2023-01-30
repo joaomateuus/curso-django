9782f7951177f0d51f07707fb8d6a990753b3a89
Joao Mateus <joao.amaral@ifam.edu.br>
2023-01-30 18:39:41 -0300

    commiting commit

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('Home')
+
+def about(request):
+    return HttpResponse('About')
+
+def contact(request):
+    return HttpResponse('Contact')
